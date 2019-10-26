62f7d20831e87eee643ecab4c1701fd564057384
Ceejaymar <martinezcj2@gmail.com>
2019-10-25 23:46:38 -0400

    Remove array from useEffect

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -14,7 +14,7 @@ const UseLocalStorageState = (key, defaultVal) => {
 
   useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(state));
-  }, [state]);
+  });
 
   return [state, setState];
 }
